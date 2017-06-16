984b2c0689c36ca255d0f3eec91e385231d07e2e
ejalalpo <el.jalalpour@gmail.com>
2017-06-15 21:43:56 -0400

    testeing the state size vs recovery

--- src/main/java/org2/Agent2/app/Agent.java
+++ src/main/java/org2/Agent2/app/Agent.java
@@ -49,7 +49,7 @@ public class Agent {
                     "->CheckIPHeader(14)" +
                     "->se::FTStateElement(ID %d, VLAN_ID %d, F %d)" +
                     "->CheckIPHeader(14)" +
-                    "->MB%d::ArrCounterMB(ID %d, 1)" +
+                    "->MB%d::ArrCounterMB(%d, 1)" +
                     "->CheckIPHeader(14)" +
                     "->[1]se;" +
                     "se[1]" +
@@ -99,7 +99,7 @@ public class Agent {
                     "->CheckIPHeader(14)" +
                     "->se::FTStateElement(ID %d, VLAN_ID %d, F %d)" +
                     "->CheckIPHeader(14)" +
-                    "->MB%d::ArrCounterMB(ID %d, 1)" +
+                    "->MB%d::ArrCounterMB(%d, 1)" +
                     "->CheckIPHeader(14)" +
                     "->[1]se;" +
                     "se[1]" +
@@ -151,7 +151,7 @@ public class Agent {
                     "->CheckIPHeader(14)" +
                     "->se::FTStateElement(ID %d, VLAN_ID %d, F %d)" +
                     "->CheckIPHeader(14)" +
-                    "->MB%d::ArrCounterMB(ID %d, 2)" +
+                    "->MB%d::ArrCounterMB(%d, 2)" +
                     "->CheckIPHeader(14)" +
                     "->[1]se;" +
                     "se[1]" +
