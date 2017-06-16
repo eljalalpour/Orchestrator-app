c6e128afe689d64532b327800535a43104335456
ejalalpo <el.jalalpour@gmail.com>
2017-06-16 12:46:54 -0400

    CheckIPHeader to MarkIPHeader

--- src/main/java/org2/Agent2/app/Agent.java
+++ src/main/java/org2/Agent2/app/Agent.java
@@ -151,7 +151,7 @@ public class Agent {
                     "->MarkIPHeader(14)" +
                     "->se::FTStateElement(ID %d, VLAN_ID %d, F %d)" +
                     "->MarkIPHeader(14)" +
-                    "->MB%d::ArrCounterMB(%d, 128)" +
+                    "->MB%d::ArrCounterMB(%d, 2)" +
                     "->MarkIPHeader(14)" +
                     "->[1]se;" +
                     "se[1]" +
